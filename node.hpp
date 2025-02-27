07c4c611d9aeb31632e8ce63efb2904772516665
JRV-XVI <jrv.software@outlook.com>
2025-02-26 19:52:46 -0600

    Node class modified

---
+++ node.hpp
@@ -0,0 +1,40 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <hash_map>
+#include <map>
+#include <ostream>
+#include <string>
+#include <string>
+
+class Node {
+public:
+  // Atributes
+  std::string name;
+  bool isFinal;
+  bool isDeathState;
+  std::map<std::string, Node> transition;
+
+  // Methods
+  Node() = default;
+
+  Node(std::string name, bool isFinal, bool isDeathState) {
+    this->name = name;
+    this->isFinal = isFinal;
+    this->isDeathState = isDeathState;
+  }
+
+  void addTransition(std::string symbol, Node node) {
+    transition[symbol] = node;
+  }
+  const Node getNextNode(const std::string symbol) {
+    return transition.at(symbol);
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const Node &node) {
+    os << "{" << node.name << node.isFinal << node.isDeathState << "}";
+    return os;
+  }
+};
+
+#endif
